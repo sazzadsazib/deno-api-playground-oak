144bdf7b454421190a7f52d6aa98ee44a92839f6
sazzdsazib <sazib66@gmail.com>
2020-05-26 10:57:09 +0600

    fix: added ts fixes

--- controllers/doctors/get.ts
+++ controllers/doctors/get.ts
@@ -1,28 +1,37 @@
-import {doctor} from './../../database.ts';
+import { doctor } from "./../../database.ts";
 
-export const doctorALL = async ({ response }:any) => {
+interface doctorItem {
+  id: any;
+  name: string;
+  doc_id: string;
+  created_at: Date;
+  hospitalName: string;
+}
+
+export const doctorALL = async ({ response }: any) => {
   try {
-    const doctorList = await doctor.find();
+    const doctorList: (null | doctorItem[] | undefined) = await doctor.find();
     response.status = 200;
-    response.body = { status: 'success', doctors: doctorList };
+    response.body = { status: "success", doctors: doctorList };
   } catch (error) {
     console.log(error);
     response.status = 500;
-    response.body = { status: 'failed', data: [] };
+    response.body = { status: "failed", data: [] };
   }
 };
 
-export const doctorSingle = async ({ response, params }:any) => {
+export const doctorSingle = async ({ response, params }: any) => {
   try {
-    let doctorResult = {};
-    if(!!params?.id){
-      doctorResult = await doctor.findOne({doc_id: params.id});
+    let doctorResult:(null | doctorItem | undefined|object) = {};
+    
+    if (!!params?.id) {
+      doctorResult = await doctor.findOne({ doc_id: params.id });
     }
     response.status = 200;
-    response.body = { status: 'success', doctors: doctorResult };
+    response.body = { status: "success", doctors: doctorResult };
   } catch (error) {
     console.log(error);
     response.status = 500;
-    response.body = { status: 'failed', data: {} };
+    response.body = { status: "failed", data: {} };
   }
 };
